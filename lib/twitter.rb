5ae25c220d768962c770bc0a3f30d00c7c2689a6
Zee Spencer <zee@zeespencer.com>
2014-06-05 22:32:44 -0700

    Explain the interface to run_app a little better

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,15 +1,16 @@
-
 ##
-# This method is the main entrance to our twitter application.
+# This method is the main entrance to our twitter application. It takes the
+# command the user wants to execute and an array containing everything else that
+# was passed in.
 #
-# Returns an array of things to print to the user.
+# It's up to you to decide what this function returns; But I'd recommend an
+# array of strings to display to the user.
 #
 # Examples:
 #   run_app("timeline", ["zspencer"])
-#   run_app("post", ["Wow!" "@CodeUnionIO" "is" "the" "greatest!"])
+#   run_app("post", ["Wow!", "@CodeUnionIO", "is", "the", "greatest!"])
 #   run_app("message", ["jfarmer", "Huh", "the", "more", "you", "know..."])
 #
 def run_app(command, arguments)
-  ["I don't do anything yet...",
-   "Maybe someday!"]
+  []
 end
