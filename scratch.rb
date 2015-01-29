1eab15f07ff469025a975c9fb72f023112e6bd20
Mark Goldenson <mjgold1@gmail.com>
2015-01-28 16:02:03 -0800

    Get timeline command working

--- scratch.rb
+++ scratch.rb
@@ -3,25 +3,43 @@ require 'simple_oauth'
 require 'excon'
 require_relative 'lib/twitter_creds'
 
+### GOALS
 
+# See any users timeline -- ./twitter timeline zspencer
+# See any users information (Join date, location, etc.) -- ./twitter info jfarmer
+# See any users followers -- ./twitter followers <yourhandle>
+# See who is following any user -- ./twitter following <yourhandle>
+# Send a tweet -- ./twitter tweet I know kung fu
+# Send a DM -- ./twitter message zspencer do you know kung fu?
+# Follow a user -- ./twitter follow jfarmer
+# Unfollow a user -- ./twitter unfollow zspencer
+
+### METHODS
+
+# Contains high-level variables and methods for a Twitter command-line client
 class Twitter
-  attr_accessible :headers, \
-                  :api_key, :api_secret, \
-                  :token, :token_secret, \
-                  :screen_name, :text, :query
+  attr_accessor :headers, \
+                :api_key, :api_secret, \
+                :token, :token_secret, \
+                :screen_name, :text, :query
 
   def initialize
-    :api_key = API_KEY
-    :api_secret = API_SECRET
-    :token = ACCESS_TOKEN
-    :token_secret = ACCESS_TOKEN_SECRET
+    @api_key = API_KEY
+    @api_secret = API_SECRET
+    @token = ACCESS_TOKEN
+    @token_secret = ACCESS_TOKEN_SECRET
   end
 
-  
+  def timeline(screen_name)
+    api_key
+  end
 
 end
 
+### TESTS
 
+twitter = Twitter.new
+p twitter.timeline 'goldenson'
 
 # authorization_header = SimpleOAuth::Header.new("post",
 # "https://api.twitter.com/1.1/direct_messages/new.json",
